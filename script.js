bd1cca72445bf4a641979f2846374e38642051eb
DamienSanchez <damien.sergio.sanchez@gmail.com>
2023-08-09 09:25:41 -0400

    feat: add new function calls function based on operator used

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-//TODO: create add, sub, multipy, and division functions
 
+let firstNum;
+let secondNum;
+let operand;
 //add
 function add(a,b){
     return a + b;
@@ -15,4 +17,16 @@ function multiply(a,b){
 //divide
 function divide(a,b){
     return a / b;
+}
+
+function operator(a,b,symbol){
+    if (symbol == '+'){
+        console.log(add(a,b))
+    }else if (symbol == '-'){
+        console.log(subtract(a,b))
+    }else if(symbol == '*'){
+        console.log(multiply(a,b))
+    }else{
+        console.log(divide(a,b))
+    }
 }
